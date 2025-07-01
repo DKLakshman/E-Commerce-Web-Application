b7b122ccec5332652fc6d96f5b1036668c173df4
P A DAHAM KAUSHIKA LAKSHMAN <116593030+DKLakshman@users.noreply.github.com>
2025-07-01 20:01:42 +0530

    create sign up page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,18 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { BrowserRouter as Router,Routes,Route} from "react-router-dom"
 import "./App.css";
+import Register from "./pages/Register";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <>
+    <Router>
+      <Routes>
+        <Route path="/register" element={<Register/>} />
+      </Routes>
      
-    </>
+    </Router>
   );
 }
 
---
+++ frontend/src/pages/Register.jsx
@@ -0,0 +1,91 @@
+import React from "react";
+// import RegisterImg from "../assets/register/RegisterImg.jpeg";
+import RegisterImg from "../assets/register/RegisterImg1.jpg";
+
+const Register = () => {
+  return (
+    <div className="bg-[#CAC7BF] min-h-screen grid grid-cols-12 items-center px-4">
+      <div className="col-span-10 md:col-span-10  lg:col-span-8 lg:col-start-3 col-start-2 md:col-start-2 h-max bg-white rounded-2xl p-8 md:p-8 grid grid-cols-2 md:grid-cols-10 lg:grid-cols-6 gap-6 mt-4">
+        <div className="col-span-2 md:col-span-5 lg:col-span-3">
+          <div className="text-[28px] font-bold pb-1 text-[#223531]">
+            Sign Up
+          </div>
+          <div className="pb-3 text-sm ">
+            Let's start with some facts about you
+          </div>
+
+          {/* First & Last Name */}
+          <div className="flex flex-col md:flex-row gap-3 pb-3">
+            <div className="w-full">
+              <p className="text-[13px] text-[#78746D]">First Name</p>
+              <input
+                className="bg-[#e6e3db] w-full h-9 rounded-lg p-2 border-[#223531] border outline-none"
+                type="text"
+              />
+            </div>
+            <div className="w-full">
+              <p className="text-[13px] text-[#78746D]">Last Name</p>
+              <input
+                className="bg-[#e6e3db] w-full h-9 rounded-lg p-2 border-[#223531] border outline-none"
+                type="text"
+              />
+            </div>
+          </div>
+
+          {/* Email */}
+          <div className="pb-3">
+            <p className="text-[13px] text-[#78746D]">Email</p>
+            <input
+              className="bg-[#e6e3db] w-full h-9 rounded-lg p-2 border-[#223531] border outline-none"
+              type="email"
+            />
+          </div>
+
+          {/* Password */}
+          <div className="pb-3">
+            <p className="text-[13px] text-[#78746D]">Password</p>
+            <input
+              className="bg-[#e6e3db] w-full h-9 rounded-lg p-2 border-[#223531] border outline-none"
+              type="password"
+            />
+          </div>
+
+          {/* Re-enter Password */}
+          <div className="pb-3">
+            <p className="text-[13px] text-[#78746D]">Re-enter Password</p>
+            <input
+              className="bg-[#e6e3db] w-full h-9 rounded-lg p-2 border-[#223531] border outline-none"
+              type="password"
+            />
+          </div>
+
+          {/* Phone */}
+          <div className="pb-3">
+            <p className="text-[13px] text-[#78746D]">Phone</p>
+            <input
+              className="bg-[#e6e3db] w-full h-9 rounded-lg p-2 border-[#223531] border outline-none"
+              type="number"
+            />
+          </div>
+
+          {/* Sign Up Button */}
+          <button className="w-full bg-[#223531] text-white h-9 rounded-lg mt-3 active:translate-y-0.5 cursor-pointer hover:bg-[#2c423d]">
+            Sign Up
+          </button>
+           <div className="text-sm text-center pt-4">Already have an account? <a href=""><u>Login</u></a> </div>
+        </div>
+
+        {/* Right Image Section */}
+        <div className="col-span-2 md:col-span-5 lg:col-span-3 hidden md:block">
+          <img
+            className="w-full h-130  rounded-xl"
+            src={RegisterImg}
+            alt="Sign Up"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Register;
